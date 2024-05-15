aa4c44fdc93b1bfd41fe94289e62152ac8d40618
sumuduhewage <sumudu.hewagee@gmail.com>
2024-05-15 17:01:45 +0800

    develop: ui changes added

--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -5,23 +5,28 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {ApplicationService} from "../services/application.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
-  displayedColumns: string[] = ['business_application_id', 'sales_agent_first_name', 'sales_agent_last_name', 'sales_agent_email', 'account_type', 'created_at', 'application_status', 'business_category', 'updated_at'];
+export class DashboardComponent implements OnInit {
+  displayedColumns: string[] = ['business_application_id', 'sales_agent_first_name', 'sales_agent_last_name', 'sales_agent_email',
+    'account_type', 'created_at', 'application_status', 'business_category', 'updated_at', 'delete'];
   dataSource = new MatTableDataSource<Application>([]);
 
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort?: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort?: MatSort;
   searchForm: FormGroup;
-  constructor(private applicationService: ApplicationService,
-  private fb: FormBuilder){}
 
-  ngOnInit():void {
+  constructor(private applicationService: ApplicationService,
+              private fb: FormBuilder,
+              private toastr: ToastrService) {
+  }
+
+  ngOnInit(): void {
     this.intiForm();
 
     this.applicationService.getAllApplications().subscribe(applications => {
@@ -33,7 +38,7 @@ export class DashboardComponent implements OnInit{
     });
   }
 
-  private intiForm() : void{
+  private intiForm(): void {
     this.searchForm = this.fb.group({
       searchText: [''],
       createdDate: [''],
@@ -45,12 +50,12 @@ export class DashboardComponent implements OnInit{
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  applyDateFilter():void {
+  applyDateFilter(): void {
 
   }
 
-  search():void {
-    const { searchText, createdAfter, updatedBefore } = this.searchForm.value;
+  search(): void {
+    const {searchText, createdAfter, updatedBefore} = this.searchForm.value;
     this.applicationService.searchByTextFieldNadCreatedUpdatedDates(searchText, createdAfter, updatedBefore).subscribe(applications => {
       this.dataSource.data = applications;
       this.dataSource.paginator = this.paginator;
@@ -58,5 +63,25 @@ export class DashboardComponent implements OnInit{
         this.dataSource.sort = this.sort;
       }
     });
+  }
+
+  deleteRecord(id: number): void {
+    if (confirm("Are you sure to delete the record?")) {
+      this.applicationService.deleteApplication(id).subscribe(response => {
+          this.dataSource.data = this.dataSource.data.filter(app => app.business_application_id !== id);
+          this.showSuccess('Item deleted Successfully')
+        },
+        error => {
+          this.showError('There was an error!')
+        })
+    }
+  }
+
+  showError(message: string) {
+    this.toastr.error(message, 'Alert!');
+  }
+
+  showSuccess(message: string) {
+    this.toastr.success(message, 'Alert!');
   }
 }
